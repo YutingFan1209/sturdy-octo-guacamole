9fc7876009ec00489426cbb2a12f3c1045b0719a
Yuting Fan <88175296+YutingFan1209@users.noreply.github.com>
2026-09-02 18:53:13 +0800

    Delete MovieShopMVC directory

--- MovieShopMVC/Services/IMovieRankingService.cs
+++
@@ -1,8 +0,0 @@
-using MovieShopMVC.Models;
-
-namespace MovieShopMVC.Services;
-
-public interface IMovieRankingService
-{
-    IReadOnlyList<Movie> GetTop30HighestGrossing(IEnumerable<Movie> movies);
-}
